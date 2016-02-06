12343f13870986b2d3d674483552b961941d981a
John Hammell <hammellj@gmail.com>
2016-02-06 08:22:31 +0100

    Changes made to correct course
    DIdn't commit them, apparently,

--- readme.md
+++ readme.md
@@ -1,8 +1,20 @@
 ## Synopsis
 
-This script fills the requirements for the course assignment for the Getting and Cleaning Data Course Project.
-It is based on the Samsung accelerometer data provided by the assignment.
-It produces four text files: Two code books and the results for points 3 and 6 in the assignment.
-It returns the summary table for the activity means for all observations.
+This script produces a summarized dataset, by subject and by activity, of the average and standard deviation of the main measurements taken using an accelerometor on a Samsung S2.
 
-Sample codebooks are provided in the repository.
+Prerequisites:
+
+plyr, dplyr and memisc libraries are required.
+
+Data must be populated beforehand.
+Dataset is located at:
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+Extract the Zip file to a sub-folder, \UCI HAR Dataset
+
+Running the script:
+
+Call source("run_analysis.R")
+
+The unsummarized data extract will be output to activity-mean-stddev.txt
+The summarized data extract will be output to activity-mean-summary.txt
--- run_analysis.R
+++ run_analysis.R
@@ -1,54 +1,55 @@
 library(plyr)
+library(dplyr)
 library(memisc)
 ##Load the X axis labels
 x_labels= read.table("UCI HAR Dataset/features.txt")
+
+##Load the subject data
+sub_train = read.table("UCI HAR Dataset/train/subject_train.txt")
+sub_test = read.table("UCI HAR Dataset/test/subject_test.txt")
+ac_labels = read.table("UCI HAR Dataset/activity_labels.txt")
 
 ##Load the X axis data
 x_train = read.table("UCI HAR Dataset/train/X_train.txt")
 x_test = read.table("UCI HAR Dataset//test/X_test.txt")
 
+##Load the Y axis data
+y_train = read.table("UCI HAR Dataset/train/y_train.txt")
+y_test = read.table("UCI HAR Dataset//test/y_test.txt")
+
 ##give clear column and row names. Point 3
+colnames(sub_train) = "activity"
+colnames(sub_test) = "activity"
+colnames(y_test) = "subject"
+colnames(y_train) = "subject"
 colnames(x_train) = x_labels$V2
 row.names(x_train) = paste( "train", row.names(x_train), sep="")
 colnames(x_test) = x_labels$V2
 row.names(x_test) = paste( "test", row.names(x_test), sep="")
 
-##Load the Y axis data
-y_train = read.table("UCI HAR Dataset/train/y_train.txt")
-y_test = read.table("UCI HAR Dataset//test/y_test.txt")
-##Load the subject data
-sub_train = read.table("UCI HAR Dataset/train/subject_train.txt")
-sub_test = read.table("UCI HAR Dataset/test/subject_test.txt")
-
 ##merge the test and training sets (point 1)
-x_test$subject = sub_test[1,]
-x_test$y = y_test[1,]
-x_train$subject = sub_train[1,]
-x_train$y = y_train[1,]
-mergedset = rbind(x_train, x_test)
+merged_test <- cbind(sub_test, y_test, x_test)
+merged_train <- cbind(sub_train, y_train, x_train)
+merged_set <- rbind(merged_test, merged_train)
 
+##Make Activity human readable
+merged_set$activity <- ac_labels$V2[merged_set$activity]
 ##clear memory
 rm(x_test, x_train, sub_train, sub_test, y_train, y_test)
 
 
 ##select only the mean and standard deviation columns. Excludes the meanFreq() variables: (point 2). point 3 above
-activity.mean.stddev = subset(observation = names(mergedset),mergedset, select = grep("(mean\\(\\)$)|(std\\(\\)$)", names(mergedset)))
+activity.mean.stddev = subset(observation = names(merged_set),merged_set, select = grep("activity|subject|(mean\\(\\)$)|(std\\(\\)$)", names(merged_set)))
 ##clean the variable names, part 4
 names(activity.mean.stddev) = gsub("\\(", "", names(activity.mean.stddev))
 names(activity.mean.stddev) = gsub("\\)", "", names(activity.mean.stddev))
+names(activity.mean.stddev) = gsub("-", ".", names(activity.mean.stddev))
+names(activity.mean.stddev) = tolower(names(activity.mean.stddev))
 
 
-
-##break the means and std into seperate sets
-means = subset(activity.mean.stddev, select = grep("mean$",names(activity.mean.stddev)))
-sds = subset(activity.mean.stddev, select = grep("std$",names(activity.mean.stddev)))
-names(means)= sub("-mean", "", names(means))
-names(sds)= sub("-std", "", names(sds))
-## put the parts back together as averages. Part 5
-meanaverage = sapply(means, function (x) mean(x))
-stddeviationaverage = sapply(sds, function (x) mean(x))
-activity.mean.summary = data.frame(activity=names(means), meanaverage, stddeviationaverage)
-rm(means, mergedset, sds, x_labels)
+##group by subject and activity and summarize, for the final step:
+activity.mean.summary = activity.mean.stddev %>% group_by(subject, activity) %>%
+  summarize_each(funs(mean))
 
 #outputting codebooks
 Write(codebook(activity.mean.stddev), file="activity-mean-stddev-cdbk.txt")
@@ -57,5 +58,5 @@ Write(codebook(activity.mean.summary), file="activity-mean-summary-cdbk.txt")
 #outputting the two tables
 write.table(activity.mean.stddev, row.name = FALSE, file="activity-mean-stddev.txt")
 write.table(activity.mean.summary, row.name = FALSE, file="activity-mean-summary.txt")
-print(activity.mean.summary)
+
 
